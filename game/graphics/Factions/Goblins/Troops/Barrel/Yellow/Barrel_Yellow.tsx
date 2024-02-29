56208c4b7b466db61db3b524ff2ea798b753d418
Spina02 <47366784+Spina02@users.noreply.github.com>
2024-02-29 16:39:05 +0100

    adding stuff

--- game/graphics/Factions/Goblins/Troops/Barrel/Yellow/Barrel_Yellow.tsx
+++ game/graphics/Factions/Goblins/Troops/Barrel/Yellow/Barrel_Yellow.tsx
@@ -1,6 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Barrel_Yellow" tilewidth="128" tileheight="128" tilecount="36" columns="6">
  <image source="Barrel_Yellow.png" width="768" height="768"/>
+ <tile id="6">
+  <animation>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="12" duration="100"/>
+  </animation>
+ </tile>
  <tile id="18">
   <animation>
    <frame tileid="0" duration="100"/>
